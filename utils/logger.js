5baf006997261139291e42e1036593eb241b2e3b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-20 05:45:18 -0500

    [fix]: prevent logger when testing

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,13 @@
 const info = (...params) => {
-  console.log(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
 };
 
 const error = (...params) => {
-  console.log(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
 };
 
 export default { info, error };
