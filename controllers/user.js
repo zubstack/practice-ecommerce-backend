225e2e5744184e4b14845e44d043e14c2ed14da0
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-09 10:32:54 -0500

    hotfix routers /users

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,16 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import User from "../models/user.js";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", async (request, response) => {
+usersRouter.get('/', async (request, response) => {
   const users = await User.find({});
   const usersList = users.map((user) => user.toJSON());
   response.json(usersList);
 });
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
   const saltRounds = 10;
@@ -27,7 +27,17 @@ usersRouter.post("/", async (request, response) => {
   response.status(201).json(savedUser);
 });
 
-usersRouter.delete("/", async (request, response) => {
-  await User.deleteMany({});
+usersRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const rta = await User.findOneAndRemove({
+    _id: id,
+  });
+
+  if (!rta) {
+    response.status(201).json('User NOT_FOUND').end();
+  } else {
+    response.status(404).json(rta).end();
+  }
 });
 export default usersRouter;
