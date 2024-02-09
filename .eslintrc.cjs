6a96f3669b4a451534c3943837117e548cb78e30
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-09 09:33:50 -0500

    linter setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,28 @@
 module.exports = {
   env: {
-    node: true,
+    browser: true,
     es2021: true,
     jest: true,
   },
-  extends: ["airbnb", "eslint:recommended"],
+  extends: 'eslint:recommended',
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "import/extensions": ["error", "ignorePackages", { "": "never" }],
-    "no-underscore-dangle": off,
-    "no-param-reassign": off,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
   },
 };
