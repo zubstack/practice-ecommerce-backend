a0ccd04730218b4c793fe46caa50674ed637b24b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-09 08:21:58 -0500

    clean tests

--- tests/reverse.test.js
+++
@@ -1,7 +0,0 @@
-import { reverseString } from "../utils/for_testing.js";
-
-describe("for_testing.js", () => {
-  test("Reverse string", () => {
-    expect(reverseString("react")).toBe("tcaer");
-  });
-});
