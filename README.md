8a003bcb51dd58af1fb18761ca84320b4e1fc20e
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-09 08:22:26 -0500

    []

--- README.md
+++ README.md
@@ -2,7 +2,3 @@
 
 Database handlers for ecommerce
 LIVE: (https://ecommerce-api-8vjd.onrender.com)
-
-# Dependencies:
-
-npm install express-async-errors // To remove try/catch syntax from our controllers: This resource will handle the exceptions (catch(errors)) under hood.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import app from "./app.js";
-import config from "./utils/config.js";
-import logger from "./utils/logger.js";
+import app from './app.js';
+import config from './utils/config.js';
+import logger from './utils/logger.js';
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
