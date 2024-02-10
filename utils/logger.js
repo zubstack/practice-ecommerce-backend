59ae06ad67631ba686bcd81d20ec97753e2a9995
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-09 21:23:50 -0500

    []

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,11 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== "test") {
+  if (process.env.NODE_ENV !== 'test') {
     console.error(...params);
   }
 };
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== "test") {
+  if (process.env.NODE_ENV !== 'test') {
     console.error(...params);
   }
 };
