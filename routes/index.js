d73fe3306aedb52e4d8465ffbb2ea00b8ee80589
zubstack <mt.calderon.alejandro@gmail.com>
2024-03-20 19:20:24 -0500

    clean useless files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import productsRouter from './products.router.js';
-import ordersRouter from './orders.router.js';
 import usersRouter from './users.router.js';
 import categoriesRouter from './categories.router.js';
 import customersRouter from './customers.router.js';
@@ -9,7 +8,6 @@ function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1/', router);
   router.use('/products', productsRouter);
-  router.use('/orders', ordersRouter);
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
   router.use('/customers', customersRouter);
